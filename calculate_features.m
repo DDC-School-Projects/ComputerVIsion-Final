8f1e11936740d92f219de69db75c3cd0cfd8912f
Peter Muller <pmm5983@rit.edu>
2015-05-07 19:48:02 -0400

    Incorporated builtin kmeans for cluster frequency generation. Added instructions at bottom for using data in neural net GUI. Results subpar right now.

--- calculate_features.m
+++ calculate_features.m
@@ -17,4 +17,36 @@ for i = 1:length(classes)
     end
 end;
 
-descriptors=descriptors';
+%descriptors=descriptors';
+
+%% generate histograms of descriptors
+k=10; %using kmeans with k=10
+hists=zeros(10,length(descriptors));
+for i=1:length(descriptors)
+    IDX=kmeans(double(descriptors(i).f'),10);
+    h = histogram(IDX);
+    hists(:,i)=(h.Values./length(IDX))'; %populate db of histograms of sift features
+end
+
+%% relate to images themselves
+%this is being hardcoded because it is specific to the data we have
+%classification: 5 classes x 341 images
+expected = zeros(5,341);
+for i=1:341
+    if strcmp(descriptors(i).class,'trilobite')
+        expected(1,i)=1;
+    elseif strcmp(descriptors(i).class,'nautilus')
+        expected(2,i)=1;
+    elseif strcmp(descriptors(i).class,'scorpion')
+        expected(3,i)=1;
+    elseif strcmp(descriptors(i).class,'sea_horse')
+        expected(4,i)=1;
+    elseif strcmp(descriptors(i).class,'stegosaurus')
+        expected(5,i)=1;
+    else
+        fprintf('Invalid!\n');
+    end
+end
+
+% once all data is calculated, use neural net wizard to train on the 
+% column matrix 'hists', with the expected values in col matrix 'expected'.
